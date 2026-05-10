f6ed0a785f3ac2ca9266002e30ebb24fda03251c
foxz1n <bryangomes16624@gmail.com>
2026-05-09 22:34:28 -0300

    feat: handle-error

---
+++ src/infraestructure/http/helpers/handle-error.ts
@@ -0,0 +1,43 @@
+import { CannotReviewOwnRestaurantError } from "@/domain/errors/cannot-review-own-restaurant.error.js";
+import { CnpjAlreadyInUseError } from "@/domain/errors/cnpj-already-in-use.error.js";
+import { CpfAlreadyInUseError } from "@/domain/errors/cpf-already-in-use.error.js";
+import { EmailAlreadyInUseError } from "@/domain/errors/email-already-in-use.error.js";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials.error.js";
+import { MenuItemUnavailableError } from "@/domain/errors/menu-item-unavailable.error.js";
+import { NotAllowedError } from "@/domain/errors/not-allowed.error.js";
+import { ResourceNotFoundError } from "@/domain/errors/resource-not-found.error.js";
+import { RestaurantAlreadyExistsError } from "@/domain/errors/restaurant-already-exists.error.js";
+import { RestaurantClosedError } from "@/domain/errors/restaurant-closed.error.js";
+import { ReviewAlreadyExistsError } from "@/domain/errors/review-already-exists.error.js";
+import type { FastifyReply } from "fastify";
+
+export function handleError(error: Error, reply: FastifyReply): FastifyReply {
+    if(error instanceof ResourceNotFoundError){
+        return reply.status(404).send({ message: error.message });
+    }
+
+    if(error instanceof NotAllowedError){
+        return reply.status(401).send({ message: error.message });
+    }
+
+    if(error instanceof InvalidCredentialsError){
+        return reply.status(401).send({ message: error.message });
+    }
+
+    if(
+        error instanceof EmailAlreadyInUseError ||
+        error instanceof CnpjAlreadyInUseError ||
+        error instanceof CpfAlreadyInUseError ||
+        error instanceof RestaurantAlreadyExistsError ||
+        error instanceof ReviewAlreadyExistsError ||
+        error instanceof CannotReviewOwnRestaurantError
+    ){
+        return reply.status(409).send({ message: error.message });
+    }
+
+    if(error instanceof RestaurantClosedError || error instanceof MenuItemUnavailableError){
+        return reply.status(422).send({ message: error.message });
+    }
+
+    throw error;
+}
