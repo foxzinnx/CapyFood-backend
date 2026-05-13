27f82b935f438ef9bbce54feaa5d5b32e04ecfae
foxz1n <bryangomes16624@gmail.com>
2026-05-13 13:54:47 -0300

    feat: order schema

---
+++ src/infraestructure/http/schemas/order.schema.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const createOrderSchema = z.object({
+    restaurantId: z.uuid(),
+    items: z.array(
+        z.object({
+            menuItemId: z.uuid(),
+            quantity: z.number().int().positive()
+        }),
+    )
+    .min(1, 'The order must contain at least one item'),
+    notes: z.string().optional()
+});
+
+export const orderQuerySchema = z.object({
+    page: z.coerce.number().int().positive().optional(),
+    perPage: z.coerce.number().int().positive().max(50).optional(),
+})
