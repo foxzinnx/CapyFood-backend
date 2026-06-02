86f4877f1edeaa5aa2d49a4d61a94300de23ce13
foxz1n <bryangomes16624@gmail.com>
2026-06-02 16:18:22 -0300

    feat: add more order errors

--- src/infraestructure/http/helpers/handle-error.ts
+++ src/infraestructure/http/helpers/handle-error.ts
@@ -3,8 +3,11 @@ import { CnpjAlreadyInUseError } from "@/domain/errors/cnpj-already-in-use.error
 import { CpfAlreadyInUseError } from "@/domain/errors/cpf-already-in-use.error.js";
 import { EmailAlreadyInUseError } from "@/domain/errors/email-already-in-use.error.js";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials.error.js";
+import { InvalidOrderStatusTransitionError } from "@/domain/errors/invalid-order-status-transition.error.js";
 import { MenuItemUnavailableError } from "@/domain/errors/menu-item-unavailable.error.js";
 import { NotAllowedError } from "@/domain/errors/not-allowed.error.js";
+import { OrderAlreadyCancelledError } from "@/domain/errors/order-already-cancelled.error.js";
+import { OrderCannotBeCancelledError } from "@/domain/errors/order-cannot-be-cancelled.error.js";
 import { ResourceNotFoundError } from "@/domain/errors/resource-not-found.error.js";
 import { RestaurantAlreadyExistsError } from "@/domain/errors/restaurant-already-exists.error.js";
 import { RestaurantClosedError } from "@/domain/errors/restaurant-closed.error.js";
@@ -35,7 +38,13 @@ export function handleError(error: Error, reply: FastifyReply): FastifyReply {
         return reply.status(409).send({ message: error.message });
     }
 
-    if(error instanceof RestaurantClosedError || error instanceof MenuItemUnavailableError){
+    if(
+        error instanceof RestaurantClosedError || 
+        error instanceof MenuItemUnavailableError ||
+        error instanceof InvalidOrderStatusTransitionError ||
+        error instanceof OrderAlreadyCancelledError ||
+        error instanceof OrderCannotBeCancelledError
+    ){
         return reply.status(422).send({ message: error.message });
     }
 
