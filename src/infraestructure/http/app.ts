ca214e3c8ded0a71571cf82b29b5cae3ef9ef475
foxz1n <bryangomes16624@gmail.com>
2026-06-06 16:19:28 -0300

    feat(app): add routes

--- src/infraestructure/http/app.ts
+++ src/infraestructure/http/app.ts
@@ -4,6 +4,13 @@ import fastifyMultipart from "@fastify/multipart";
 import fastifyStatic from "@fastify/static";
 import fastify from "fastify";
 import { resolve } from "node:path";
+import { restaurantOwnerRoutes } from "./routes/restaurant-owner.routes.js";
+import { restaurantRoutes } from "./routes/restaurant.routes.js";
+import { menuItemRoutes } from "./routes/menu-item.routes.js";
+import { customerRoutes } from "./routes/customer.routes.js";
+import { orderRoutes } from "./routes/order.routes.js";
+import { reviewRoutes } from "./routes/review.routes.js";
+import { ZodError } from "zod";
 
 export async function buildApp(){
     const app = fastify({ logger: true });
@@ -28,6 +35,35 @@ export async function buildApp(){
             prefix: '/uploads'
         });
     }
+
+    await app.register(restaurantOwnerRoutes, { prefix: '/owners' });
+    await app.register(restaurantRoutes, { prefix: '/restaurants' });
+    await app.register(menuItemRoutes, { prefix: '/restaurants' });
+    await app.register(customerRoutes, { prefix: '/customers' });
+    await app.register(orderRoutes, { prefix: '/orders' });
+    await app.register(reviewRoutes, { prefix: '/restaurants' });
+
+    app.setErrorHandler((error, _, reply) => {
+        if(error instanceof ZodError){
+            return reply.status(422).send({
+                message: 'Validation error',
+                errors: error.flatten().fieldErrors
+            })
+        }
+
+        const err = error as any;
+
+        if(err.statusCode){
+            return reply.status(err.statusCode).send({
+                message: err.message
+            })
+        }
+
+        app.log.error(error);
+        return reply.status(500).send({
+            message: 'Internal server error'
+        })
+    })
 
     return app;
 }
